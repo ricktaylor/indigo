64b1144d99bc1ea6e78e39d229a2f4383fb38c0b
Rick Taylor <rick@tropicalstormsoftware.com>
2013-06-10 16:16:04 +0100

    Add ooxml submodule
    Signed-off-by: Rick Taylor <rick@tropicalstormsoftware.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,12 @@ libtoolize --force --no-warn
 automake --foreign --add-missing
 autoconf
 
-#echo Bootstrapping OOBase...
-#cd oobase
-#./bootstrap.sh
-#cd ..
+echo Bootstrapping OOBase...
+cd oobase
+./bootstrap.sh
+cd ..
+
+echo Bootstrapping OOXML...
+cd ooxml
+./bootstrap.sh
+cd ..
