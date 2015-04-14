0b6b7cb7312763c518413c4929e17935f4776079
Rick Taylor <rick@tropicalstormsoftware.com>
2015-04-14 11:16:24 +0100

    Tweak bin2c to fix extern issue
    Signed-off-by: Rick Taylor <rick@tropicalstormsoftware.com>

--- tools/bin2c.c
+++ tools/bin2c.c
@@ -56,7 +56,8 @@ int main( int argc, char* argv[])
 
     need_comma = 0;
 
-    fprintf (f_output, "const unsigned char %s[%i] = \n{", ident, file_size);
+    fprintf (f_output, "#include <stddef.h>\n");
+    fprintf (f_output, "static const unsigned char s_%s[%i] = \n{", ident, file_size);
     for (i = 0; i < file_size; ++i)
     {
         if (need_comma) fprintf(f_output, ",");
@@ -65,6 +66,8 @@ int main( int argc, char* argv[])
         fprintf(f_output, "0x%.2x", buf[i] & 0xff);
     }
     fprintf(f_output, "\n};\n");
+    fprintf (f_output, "const unsigned char* %s = s_%s;\n", ident, ident);
+    fprintf (f_output, "const size_t %s_length = sizeof(s_%s);\n", ident, ident);
 
     fclose(f_output);
 
