f6501941a4cb7986803488016783642bedc3bedf
Rick Taylor <rick@tropicalstormsoftware.com>
2016-05-23 18:02:32 +0100

    Use autoreconf in bootstrap
    Signed-off-by: Rick Taylor <rick@tropicalstormsoftware.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,3 @@
 #! /bin/sh
 echo Running autotools...
-
-mkdir -p m4
-aclocal -I m4
-autoheader
-libtoolize --force --no-warn
-automake --foreign --add-missing
-autoconf
-
-echo Bootstrapping OOBase...
-cd oobase
-./bootstrap.sh
-cd ..
+autoreconf -fvi
