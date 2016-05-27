fff88fdaf4c72ab7cbf070e7f1d8f69a5cad22df
Rick Taylor <rick@tropicalstormsoftware.com>
2016-05-27 18:04:22 +0100

    Add UIDialog
    Signed-off-by: Rick Taylor <rick@tropicalstormsoftware.com>

---
+++ include/indigo/ui/UIDialog.h
@@ -0,0 +1,48 @@
+///////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (C) 2016 Rick Taylor
+//
+// This file is part of the Indigo boardgame engine.
+//
+// OOGL is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OOGL is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with OOGL.  If not, see <http://www.gnu.org/licenses/>.
+//
+///////////////////////////////////////////////////////////////////////////////////
+
+#ifndef INDIGO_UIDIALOG_H_INCLUDED
+#define INDIGO_UIDIALOG_H_INCLUDED
+
+#include "../indigo/include/indigo/ui/UILayer.h"
+
+namespace Indigo
+{
+	class UIDialog
+	{
+	protected:
+		UIDialog(const OOBase::SharedPtr<UILayer>& layer);
+
+		void internal_do_modal();
+
+		void end_dialog();
+
+		virtual void on_window_close() = 0;
+
+	private:
+		OOBase::SharedPtr<UILayer> m_layer;
+		bool                       m_live;
+
+		void window_close(const Window& w);
+	};
+}
+
+#endif // INDIGO_UIDIALOG_H_INCLUDED
---
+++ src/ui/UIDialog.cpp
@@ -0,0 +1,63 @@
+///////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (C) 2016 Rick Taylor
+//
+// This file is part of the Indigo boardgame engine.
+//
+// Indigo is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Indigo is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Indigo.  If not, see <http://www.gnu.org/licenses/>.
+//
+///////////////////////////////////////////////////////////////////////////////////
+
+#include "../Common.h"
+
+#include "../../include/indigo/ui/UIDialog.h"
+#include "../../include/indigo/ui/UILayer.h"
+
+Indigo::UIDialog::UIDialog(const OOBase::SharedPtr<UILayer>& layer) :
+		m_layer(layer),
+		m_live(false)
+{
+	if (!m_layer)
+		LOG_WARNING(("No layer assigned to quit dialog"));
+
+
+}
+
+void Indigo::UIDialog::internal_do_modal()
+{
+	if (m_layer)
+	{
+		OOBase::Delegate1<void,const Indigo::Window&,OOBase::ThreadLocalAllocator> prev_close = m_layer->window()->on_close(OOBase::make_delegate<OOBase::ThreadLocalAllocator>(this,&UIDialog::window_close));
+
+		m_layer->show();
+
+		for (m_live = true;m_live;)
+			thread_pipe()->get();
+
+		m_layer->window()->on_close(prev_close);
+
+		m_layer->show(false);
+	}
+}
+
+void Indigo::UIDialog::end_dialog()
+{
+	m_live = false;
+}
+
+void Indigo::UIDialog::window_close(const Window& w)
+{
+	if (m_layer->window().get() == &w)
+		this->on_window_close();
+}
