ea1e123ca2f6e730ea0ad7c00f86efdd49bcbbe1
Rick Taylor <rick@tropicalstormsoftware.com>
2016-06-02 18:12:48 +0100

    Add SGCamera
    Signed-off-by: Rick Taylor <rick@tropicalstormsoftware.com>

---
+++ include/indigo/sg/SGCamera.h
@@ -0,0 +1,41 @@
+///////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (C) 2016 Rick Taylor
+//
+// This file is part of the Indigo boardgame engine.
+//
+// Indigo is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Indigo is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Indigo.  If not, see <http://www.gnu.org/licenses/>.
+//
+///////////////////////////////////////////////////////////////////////////////////
+
+#ifndef INDIGO_SGCAMERA_H_INCLUDED
+#define INDIGO_SGCAMERA_H_INCLUDED
+
+#include "SGNode.h"
+
+namespace Indigo
+{
+	namespace Render
+	{
+
+	}
+
+	class SGCamera : public SGNode
+	{
+	public:
+
+	};
+}
+
+#endif // INDIGO_SGCAMERA_H_INCLUDED
