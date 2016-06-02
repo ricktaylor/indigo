ee45a324af80281d7d9a350286f6f97020cae210
Rick Taylor <rick@tropicalstormsoftware.com>
2016-06-02 15:37:12 +0100

    Add lots of extra stuff to SGNode
    Signed-off-by: Rick Taylor <rick@tropicalstormsoftware.com>

--- include/indigo/sg/SGNode.h
+++ include/indigo/sg/SGNode.h
@@ -24,8 +24,11 @@
 
 #include <OOBase/SharedPtr.h>
 
+#include <glm/gtc/quaternion.hpp>
+
 namespace Indigo
 {
+	class SGNode;
 	class SGGroup;
 
 	namespace Render
@@ -35,31 +38,45 @@ namespace Indigo
 		class SGNode : public OOBase::NonCopyable
 		{
 		public:
+			virtual ~SGNode() {}
+
 			virtual bool valid() const { return true; }
 
 			SGGroup* parent() const { return m_parent; }
 
-			bool is_dirty() const { return m_dirty; }
-			virtual void update();
+			void show(bool visible = true);
+
+			bool is_dirty() const { return m_state & eRSG_dirty; }
+			void make_dirty();
+
+			void local_transform(glm::mat4 transform);
 
 		protected:
 			SGNode(SGGroup* parent);
 
-			virtual ~SGNode() {}
-
-			void make_dirty();
+			virtual void update(const glm::mat4& parent_transform);
 
 		private:
-			bool     m_dirty;
-			SGGroup* m_parent;
+			enum State
+			{
+				eRSG_dirty = 1,
+				eRSG_visible = 2,
+			};
+			unsigned int m_state;
+			SGGroup*     m_parent;
+			glm::mat4    m_local_transform;
+			glm::mat4    m_world_transform;
 		};
 
 		class SGGroup : public SGNode
 		{
 			friend class Indigo::SGGroup;
 
-		public:
-			
+		protected:
+			virtual void update(const glm::mat4& parent_transform) = 0;
+
+		private:
+			void attach_node(Indigo::SGNode* node, bool* ret);
 		};
 	}
 
@@ -69,36 +86,87 @@ namespace Indigo
 		friend class Render::SGGroup;
 
 	public:
-		virtual ~SGNode();
+		enum State
+		{
+			eNS_visible = 0x1,
+			eNS_enabled = 0x2
+		};
+
+		struct CreateParams
+		{
+			CreateParams(OOBase::uint32_t state = 0
+			) :
+				m_state(state)
+			{}
+
+			OOBase::uint32_t m_state;
+		};
+
+		virtual ~SGNode() {}
 
 		SGGroup* parent() const { return m_parent; }
 
-		virtual bool valid() const { return true; }
+		virtual bool valid() const { return m_render_node; }
+
+		bool visible() const { return valid() && (m_state & eNS_visible); }
+		virtual void show(bool visible = true);
+
+		bool enabled() const { return valid() && (m_state & eNS_enabled); }
+		void enable(bool enabled = true);
+
+		OOBase::uint32_t state() const { return m_state; }
+		void toggle_state(OOBase::uint32_t new_state, OOBase::uint32_t mask);
+		void toggle_state(bool on, OOBase::uint32_t mask) { toggle_state(on ? mask : 0,mask); }
+
+		const glm::vec3& position() const { return m_position; }
+		void position(const glm::vec3& pos);
+
+		const glm::vec3& scaling() const { return m_scaling; }
+		void scaling(const glm::vec3& scale);
+
+		const glm::quat& rotation() const { return m_rotation; }
+		void rotation(const glm::quat& rot);
 
 	protected:
-		SGNode(SGNode* parent);
+		SGNode(SGGroup* parent, const CreateParams& params = CreateParams());
 
-		virtual bool on_render_create(Render::SGGroup* parent) = 0;
+		Render::SGNode* render_node() const { return m_render_node; }
+
+		virtual Render::SGNode* on_render_create(Render::SGGroup* parent) = 0;
+
+		virtual void on_state_change(OOBase::uint32_t state, OOBase::uint32_t change_mask);
 
 	private:
-		SGGroup* m_parent;		
+		SGGroup*         m_parent;
+		Render::SGNode*  m_render_node;
+		OOBase::uint32_t m_state;
+		glm::vec3        m_position;
+		glm::vec3        m_scaling;
+		glm::quat        m_rotation;
+
+		glm::mat4 transform() const;
 	};
 
 	class SGGroup : public SGNode
 	{
-	public:
-
 	protected:
-		Render::SGGroup* m_render_group;
+		SGGroup(SGGroup* parent, const CreateParams& params = CreateParams()) : SGNode(parent,params)
+		{}
 
 		bool attach_node(const OOBase::SharedPtr<SGNode>& node);
-		
-	private:		
 	};
 
 	class SGSimpleGroup : public SGGroup
 	{
+	public:
+		SGSimpleGroup(SGGroup* parent = NULL, const CreateParams& params = CreateParams()) : SGGroup(parent,params)
+		{}
 
+		bool add_node(const OOBase::SharedPtr<SGNode>& node);
+		bool remove_node(const OOBase::SharedPtr<SGNode>& node);
+
+	private:
+		OOBase::Vector<OOBase::SharedPtr<SGNode>,OOBase::ThreadLocalAllocator> m_children;
 	};
 }
 
--- src/sg/SGNode.cpp
+++ src/sg/SGNode.cpp
@@ -22,23 +22,160 @@
 #include "../Common.h"
 #include "../../include/indigo/sg/SGNode.h"
 
+#include "../../include/indigo/Render.h"
+
 Indigo::Render::SGNode::SGNode(SGGroup* parent) :
+		m_state(eRSG_dirty),
 		m_parent(parent)
 {
+	make_dirty();
+}
+
+void Indigo::Render::SGNode::show(bool visible)
+{
+	if (visible)
+		m_state |= eRSG_visible;
+	else
+		m_state &= ~eRSG_visible;
 }
 
 void Indigo::Render::SGNode::make_dirty()
 {
-	if (!m_dirty)
+	if (!(m_state & eRSG_dirty))
 	{
-		m_dirty = true;
+		m_state |= eRSG_dirty;
 
 		if (m_parent)
 			m_parent->make_dirty();
 	}
 }
 
-void Indigo::Render::SGNode::update()
+void Indigo::Render::SGNode::update(const glm::mat4& parent_transform)
 {
-	m_dirty = false;
+	m_world_transform = parent_transform * m_local_transform;
+
+	m_state &= ~eRSG_dirty;
+}
+
+void Indigo::Render::SGNode::local_transform(glm::mat4 transform)
+{
+	if (m_local_transform != transform)
+	{
+		m_local_transform = transform;
+
+		make_dirty();
+	}
+}
+
+void Indigo::Render::SGGroup::attach_node(Indigo::SGNode* node, bool* ret)
+{
+	node->m_render_node = node->on_render_create(this);
+	*ret = (node->m_render_node != NULL);
+	if (*ret)
+		make_dirty();
+}
+
+Indigo::SGNode::SGNode(SGGroup* parent, const CreateParams& params) :
+		m_parent(parent),
+		m_render_node(NULL),
+		m_state(params.m_state)
+{
+}
+
+void Indigo::SGNode::toggle_state(OOBase::uint32_t new_state, OOBase::uint32_t mask)
+{
+	OOBase::uint32_t change_mask = (m_state ^ new_state) & mask;
+	if (change_mask)
+	{
+		m_state = (m_state & ~mask) | (new_state & mask);
+
+		on_state_change(m_state,change_mask);
+	}
+}
+
+void Indigo::SGNode::on_state_change(OOBase::uint32_t state, OOBase::uint32_t change_mask)
+{
+	if (change_mask & eNS_visible)
+	{
+		bool visible = (state & eNS_visible) == eNS_visible;
+		if (m_render_node)
+			render_pipe()->post(OOBase::make_delegate(m_render_node,&Render::SGNode::show),visible);
+	}
+}
+
+void Indigo::SGNode::show(bool visible)
+{
+	toggle_state(visible,eNS_visible);
+}
+
+void Indigo::SGNode::enable(bool enable)
+{
+	toggle_state(enable,eNS_enabled);
+}
+
+glm::mat4 Indigo::SGNode::transform() const
+{
+	return glm::scale(glm::mat4_cast(m_rotation) * glm::translate(glm::mat4(),m_position),m_scaling);
+}
+
+void Indigo::SGNode::position(const glm::vec3& pos)
+{
+	if (m_position != pos)
+	{
+		m_position = pos;
+
+		if (m_render_node)
+			render_pipe()->post(OOBase::make_delegate(m_render_node,&Render::SGNode::local_transform),transform());
+	}
+}
+
+void Indigo::SGNode::scaling(const glm::vec3& scale)
+{
+	if (m_scaling != scale)
+	{
+		m_scaling = scale;
+
+		if (m_render_node)
+			render_pipe()->post(OOBase::make_delegate(m_render_node,&Render::SGNode::local_transform),transform());
+	}
+}
+
+void Indigo::SGNode::rotation(const glm::quat& rot)
+{
+	if (m_rotation != rot)
+	{
+		m_rotation = rot;
+
+		if (m_render_node)
+			render_pipe()->post(OOBase::make_delegate(m_render_node,&Render::SGNode::local_transform),transform());
+	}
+}
+
+bool Indigo::SGGroup::attach_node(const OOBase::SharedPtr<SGNode>& node)
+{
+	if (!m_render_node)
+		LOG_ERROR_RETURN(("Failed to insert node: incomplete group"),false);
+
+	bool ret = false;
+	render_pipe()->call(OOBase::make_delegate(static_cast<Indigo::Render::SGGroup*>(m_render_node),&Render::SGGroup::attach_node),node.get(),&ret);
+	return ret;
+}
+
+bool Indigo::SGSimpleGroup::add_node(const OOBase::SharedPtr<SGNode>& node)
+{
+	if (!m_children.push_back(node))
+		LOG_ERROR_RETURN(("Failed to insert node: %s",OOBase::system_error_text()),false);
+
+	if (!attach_node(node))
+	{
+		m_children.pop_back();
+		return false;
+	}
+
+	return true;
+}
+
+bool Indigo::SGSimpleGroup::remove_node(const OOBase::SharedPtr<SGNode>& node)
+{
+	return m_children.remove(node) != 0;
 }
